f06d2482229a17bd807431a9405f639eaf64919f
aissa louche <iamaissauc@gmail.com>
2021-03-10 18:02:17 +0100

    add config file & autoload core libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,16 @@
 <?php
 
-// Load libraries files
-require_once('libraries/Core.php');
-require_once('libraries/Controller.php');
-require_once('libraries/Database.php');
+// // Load libraries files
+
+// require_once('libraries/Core.php');
+// require_once('libraries/Controller.php');
+// require_once('libraries/Database.php');
+
+
+// Autoloader Core libraries
+spl_autoload_register(function($className) {
+    
+    require_once('libraries/'. $className .'.php');
+
+});
+
---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//App root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+//Url Root
+define('URL_ROOT', 'http://localhost/sharedpost');
+
+// Site name
+define('SITE_NAME', 'Sharedpost');
+
+
