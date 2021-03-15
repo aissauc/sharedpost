2046c91f1c5a47442d524aca8c9d683921cf150d
aissa louche <iamaissauc@gmail.com>
2021-03-15 19:17:10 +0100

    create database class

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,12 @@
 <?php
 
+// Databae params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'sharedpost');
+
+
 //App root
 define('APP_ROOT', dirname(dirname(__FILE__)));
 
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,14 +4,15 @@ class Pages extends Controller {
 
     public function __construct() {
 
-        
+        $this->postModel = $this->model('Post');
 
     }
 
     public function index() {
-
+        $posts = $this->postModel->getPosts();
         $data = [
-            'title' => 'website'
+            'title' => 'website',
+            'posts' => $posts
         ];
 
         $this->view('pages/index', $data);
