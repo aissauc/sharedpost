a7d04b8467f4b863578913c62bfd9412036262f4
aissa louche <iamaissauc@gmail.com>
2021-03-16 20:07:16 +0100

    create users controller

---
+++ app/controllers/Users.php
@@ -0,0 +1,27 @@
+<?php
+
+class Users extends Controller {
+
+	public function register() {
+		// Check for post
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			// Proccess form
+		} else {
+			// Init data
+			$data = [
+				'name' => '',
+				'email' => '',
+				'password' => '',
+				'confirm_password' => '',
+				'name_err' => '',
+				'email_err' => '',
+				'password_err' => '',
+				'confirm_password_err' => ''	
+			];
+			// LOad the view
+			$this->view('users/register');
+		}
+
+	}
+
+}
